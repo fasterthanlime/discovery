3ceafe0bd0dcd5d27a53b1a17da65ec1ad7e4320
Amos Wenger <amoswenger@gmail.com>
2023-02-01 16:06:04 +0100

    led-roulette it blinks

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,31 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use microbit as _;
-use panic_halt as _;
+use microbit::{
+    self as _,
+    hal::{prelude::*, timer::Timer},
+    Board,
+};
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
+    let mut board = Board::take().unwrap();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut timer = Timer::new(board.TIMER0);
+
+    board.display_pins.col1.set_low().unwrap();
+    let mut row1 = board.display_pins.row1;
+
+    loop {
+        row1.set_low().unwrap();
+        rprintln!("Dark!");
+        timer.delay_ms(1_000_u16);
+
+        row1.set_high().unwrap();
+        rprintln!("Light!");
+        timer.delay_ms(1_000_u16);
+    }
 }
